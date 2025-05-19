5f3d8233ee91436cd6be383bd5dee755ea936088
ducvietdev <vietnho2004@gmail.com>
2025-05-19 17:25:40 +0700

    fix8

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,16 +1,33 @@
-import jwt from "jsonwebtoken";
+// import jwt from "jsonwebtoken";
 
-export const generateToken = (userId , res) => {
+// export const generateToken = (userId , res) => {
+//     const token = jwt.sign(
+//         {_id: userId}, 
+//         process.env.JWT_SECRET, 
+//         {expiresIn: '2d'}
+//     )
+//     res.cookie('jwt', token, {
+//         maxAge: 2*24*60*60*1000,
+//         httpOnly: true,
+//         sameSite: 'Lax',
+//         secure: process.env.NODE_ENV !== 'development'
+//     })
+//     return token
+// }
+
+export const generateToken = (userId, res) => {
     const token = jwt.sign(
-        {_id: userId}, 
-        process.env.JWT_SECRET, 
-        {expiresIn: '2d'}
-    )
+        { _id: userId },
+        process.env.JWT_SECRET,
+        { expiresIn: '2d' }
+    );
+
     res.cookie('jwt', token, {
-        maxAge: 2*24*60*60*1000,
         httpOnly: true,
-        sameSite: 'Lax',
-        secure: process.env.NODE_ENV !== 'development'
-    })
-    return token
-}
+        secure: true,           
+        sameSite: 'None',       
+        maxAge: 2 * 24 * 60 * 60 * 1000
+    });
+
+    return token;
+};
